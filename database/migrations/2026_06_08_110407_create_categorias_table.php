05f237a52019e8a6d0059d5b47d563b7d8c58c6f
RivLaura <271614915+RivLaura@users.noreply.github.com>
2026-06-08 10:29:50 -0300

    Fiamb 33 crear seeder categoria (#6)
    * FIAMB-33 Crear seeder Categoria
    
    * FIAMB-33 Crear seeder Categoria
    
    * Solucionado error Seeder

--- database/migrations/2026_06_08_110407_create_categorias_table.php
+++ database/migrations/2026_06_08_110407_create_categorias_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('categorias', function (Blueprint $table) {
             $table->id();
             $table->string('nombre', 100)->unique();
-            $table->text('descripcion');
+            $table->text('descripcion')->nullable();
             $table->timestamps();
         });
     }
---
+++ database/seeders/CategoriaSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Categoria;
+
+class CategoriaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categorias = [
+            'Fiambres',
+            'Quesos',
+            'Embutidos',
+            'Bebidas',
+        ];
+
+        foreach ($categorias as $nombre) {
+            Categoria::create([
+                'nombre' => $nombre,
+                'descripcion' => null,
+            ]);
+        }
+    }
+}
