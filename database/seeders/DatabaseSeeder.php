60d2cb1824d852e4125c96322d0d1b1b8c29ad88
RivLaura <rmlaura261114@gmail.com>
2026-06-08 10:15:10 -0300

    FIAMB-33 Crear seeder Categoria

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // usuario de prueba
 
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        // seed de categorias
+        
+        $this->call([
+            CategoriaSeeder::class,
+        ]);
     }
 }
+
