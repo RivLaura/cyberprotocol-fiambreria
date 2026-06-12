1e66562b90b86d86cd552af6e1030c7980ecb4b8
Alejandro Quiroga <aquiroga888@gmail.com>
2026-06-13 00:54:43 +0200

    FIAMB-38 Crear formulario alta de categorías e integrar campo descripcion en rutas y controlador

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,18 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+   
+ // CATEGORÍAS
     Route::get('/categorias', [CategoriaController::class, 'index'])
-    ->name('categorias.index');
+        ->name('categorias.index');
 
-    
+    Route::get('/categorias/crear', [CategoriaController::class, 'create'])
+        ->name('categorias.create');
+
+    Route::post('/categorias', [CategoriaController::class, 'store'])
+        ->name('categorias.store');
 });
+    
+
 
 require __DIR__.'/auth.php';
