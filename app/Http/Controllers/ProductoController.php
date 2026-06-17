e7880f7590e418561008729d0dffade08fa1c18b
Alejandro Quiroga <aquiroga888@gmail.com>
2026-06-17 12:08:58 +0200

    FIAMB-111: corregido metodo index en ProductoController para enviar stock a la vista

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -13,7 +13,9 @@ class ProductoController extends Controller
      */
     public function index()
     {
-        //
+        $productos = Producto::with('categoria')->get();
+
+        return view('productos.index', compact('productos'));
     }
 
     /**
