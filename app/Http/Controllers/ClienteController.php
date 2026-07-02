0aad5525c6e4d4353171183047795d99c1c93fa7
MrPhoenix <nieva.cronos@gmail.com>
2026-07-02 01:09:35 -0300

    FIAMB-147 Implementar paginación del listado de clientes. (#79)

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -2,40 +2,75 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cliente;
 use Illuminate\Http\Request;
 
 class ClienteController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     */
     public function index(Request $request)
     {
-        //
+        $buscar = $request->input('buscar');
+
+        $clientes = Cliente::query()
+            ->when($buscar, function ($query) use ($buscar) {
+                $query->where('nombre', 'like', "%{$buscar}%")
+                    ->orWhere('apellido', 'like', "%{$buscar}%")
+                    ->orWhere('telefono', 'like', "%{$buscar}%")
+                    ->orWhere('email', 'like', "%{$buscar}%");
+            })
+            ->orderBy('nombre', 'asc')
+            ->paginate(10)
+            ->withQueryString();
+
+        return view('clientes.index', compact('clientes'));
     }
 
+    /**
+     * Show the form for creating a new resource.
+     */
     public function create()
     {
         //
     }
 
+    /**
+     * Store a newly created resource in storage.
+     */
     public function store(Request $request)
     {
         //
     }
 
+    /**
+     * Display the specified resource.
+     */
     public function show(string $id)
     {
         //
     }
 
+    /**
+     * Show the form for editing the specified resource.
+     */
     public function edit(string $id)
     {
         //
     }
 
+    /**
+     * Update the specified resource in storage.
+     */
     public function update(Request $request, string $id)
     {
         //
     }
 
+    /**
+     * Remove the specified resource from storage.
+     */
     public function destroy(string $id)
     {
         //
